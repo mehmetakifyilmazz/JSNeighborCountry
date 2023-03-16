7c4aa8f6a8dddcd9c199b435a43bea742587c94e
Mehmet Akif Yılmaz <124696212+mehmetakifyilmazz@users.noreply.github.com>
2023-03-16 11:13:25 +0300

    Add files via upload

--- countries.js
+++ countries.js
@@ -8,12 +8,12 @@ async function getCountry(country) {
     try {
         const response = await fetch('https://restcountries.com/v3.1/name/' + country);
         if(!response.ok)
-            throw new Error("Ülke bulunamadı.");
+            throw new Error("COUNTRY NOT FOUND.");
         const data = await response.json();
         renderCountry(data[0]);
         const countries = data[0].borders;
         if(!countries) 
-            throw new Error("Komşu ülke bulunamadı.");
+            throw new Error("NEIGHBOR COUNTRY NOT FOUND.");
         const response2 = await fetch('https://restcountries.com/v3.1/alpha?codes=' + countries.toString());
         const neighbors = await response2.json();
         renderNeighbors(neighbors);
@@ -35,19 +35,19 @@ function renderCountry(data) {
                 <h3 class="card-title">${data.name.common}</h3>
                 <hr>
                 <div class="row">
-                    <div class="col-4">Nufüs: </div>
+                    <div class="col-4">Population: </div>
                     <div class="col-8">${(data.population / 1000000).toFixed(1)} milyon</div>
                 </div>
                 <div class="row">
-                    <div class="col-4">Resmi Dil: </div>
+                    <div class="col-4">Language: </div>
                     <div class="col-8">${Object.values(data.languages)}</div>
                 </div>
                 <div class="row">
-                    <div class="col-4">Başkent: </div>
+                    <div class="col-4">Capital: </div>
                     <div class="col-8">${data.capital[0]}</div>
                 </div>
                 <div class="row">
-                    <div class="col-4">Para Birimi: </div>
+                    <div class="col-4">Currency: </div>
                     <div class="col-8">${Object.values(data.currencies)[0].name} (${Object.values(data.currencies)[0].symbol})</div>
                 </div>
             </div>
